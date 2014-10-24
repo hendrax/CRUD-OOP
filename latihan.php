907cf069e3a9a23c8d7ac6f14cc676c44ed69a2e
hendrax <hendra1602@gmail.com>
2014-10-24 14:40:39 +0700

    commit 1, hendra

---
+++ latihan.php
@@ -0,0 +1,14 @@
+<!--
+file : latihan.php
+oleh : hendra
+-->
+<html>
+   <head>
+      <title>Latihan Github</title>
+   </head>
+   <body>
+      <h1>Latihan Github</h1>
+      <hr>
+      <p>bla bla bla...</p>
+   </body>
+</html>
